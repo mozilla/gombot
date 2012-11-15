6d279102baaa40d68ceddb8e66fba3a5835b9af1
Zachary Carter <zack.carter@gmail.com>
2012-11-14 16:35:24 -0800

    use a common config file

---
+++ etc/config.js
@@ -0,0 +1,31 @@
+
+var config = module.exports = {
+  public_url: process.env.PUBLIC_URL || 'http://127.0.0.1:20000',
+  process: {
+    router: {
+      port: process.env.PORT || 20000,
+      host: '127.0.0.1'
+    },
+    api: {
+      port: 20001,
+      host: '127.0.0.1'
+    },
+    static: {
+      port: 20002,
+      host: '127.0.0.1'
+    }
+  },
+  hapi: {
+    name: "Gombot API Server",
+    docs: true
+  },
+  db: {
+    hosts: [ 'localhost:8091' ],
+    username: 'Administration',
+    password: process.env.DB_PASSWORD || null,
+    bucket: process.env.DB_BUCKET || 'default'
+  }
+};
+
+config.api_url = 'http://' + config.process.api.host + ':' + config.process.api.port;
+config.static_url = 'http://' + config.process.static.host + ':' + config.process.static.port;
