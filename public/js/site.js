574b9b80e205b8048448ea3f696a562434569a3c
Zachary Carter <zack.carter@gmail.com>
2013-01-02 11:59:25 -0800

    add inline install script

--- public/js/site.js
+++ public/js/site.js
@@ -1,41 +1,59 @@
 (function() {
-    var site = {
-        platform: 'windows'
-    };
+  var site = {
+    platform: 'windows'
+  };
 
-    if(navigator.platform.indexOf("Win32") != -1 ||
-       navigator.platform.indexOf("Win64") != -1) {
-        site.platform = 'windows';
-    }
-    else if (navigator.platform.indexOf("armv7l") != -1) {
-        site.platform = 'android';
-    }
-    else if(navigator.platform.indexOf("Linux") != -1) {
-        site.platform = 'linux';
-    }
-    else if (navigator.userAgent.indexOf("Mac OS X") != -1) {
-        site.platform = 'osx';
-    }
-    else if (navigator.userAgent.indexOf("MSIE 5.2") != -1) {
-        site.platform = 'osx';
-    }
-    else if (navigator.platform.indexOf("Mac") != -1) {
-        site.platform = 'mac';
-    }
-    else {
-        site.platform = 'other';
-    }
+  if (navigator.platform.indexOf("Win32") != -1 ||
+     navigator.platform.indexOf("Win64") != -1) {
+    site.platform = 'windows';
+  } else if (navigator.platform.indexOf("armv7l") != -1) {
+    site.platform = 'android';
+  } else if(navigator.platform.indexOf("Linux") != -1) {
+    site.platform = 'linux';
+  } else if (navigator.userAgent.indexOf("Mac OS X") != -1) {
+    site.platform = 'osx';
+  } else if (navigator.userAgent.indexOf("MSIE 5.2") != -1) {
+    site.platform = 'osx';
+  } else if (navigator.platform.indexOf("Mac") != -1) {
+    site.platform = 'mac';
+  } else {
+    site.platform = 'other';
+  }
 
-    function init() {
-	// Add the platform as a classname on the html-element immediately to avoid lots
-        // of flickering
-	var h = document.documentElement;
-	h.className = h.className.replace("windows", site.platform);
+  function init() {
+    // Add the platform as a classname on the html-element immediately to avoid lots
+    // of flickering
+    var h = document.documentElement;
+    h.className = h.className.replace("windows", site.platform);
 
-        // Add class to reflect javascript availability for CSS
-        h.className = h.className.replace(/\bno-js\b/,'js');
-    }
+    // Add class to reflect javascript availability for CSS
+    h.className = h.className.replace(/\bno-js\b/,'js');
+  }
 
-    init();
-    window.site = site;
+  init();
+  window.site = site;
+
+  $(document).ready(function() {
+
+    // set up inline install
+    var btn = $('#download-button');
+    var installUrl = $('#chrome-alpha-dl').attr('href');
+
+    btn.click(function(e) {
+      if (btn.hasClass('chrome') && typeof chrome !== 'undefined') {
+        e.preventDefault();
+        chrome.webstore.install(
+          installUrl,
+          function() {
+            console.log('install success');
+          },
+          function(err) {
+            console.log('install error:', err);
+          }
+        );
+      }
+    });
+
+  });
+
 })();
